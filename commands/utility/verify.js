afedfa30d92e5ded7f6c610949080d2a34d7fcb5
Daniel Dugovic <ddugovic@users.noreply.github.com>
2024-01-06 14:24:13 -0600

    Use ephemeral responses

--- commands/utility/verify.js
+++ commands/utility/verify.js
@@ -1,43 +1,43 @@
 const { SlashCommandBuilder } = require('discord.js');
-const {getUserInfo} = require("../../checkGithubUser");
-const {owner} = require("../../config.json");
-const {roleId, collabUsersIds} = require("../../config.json")
-const {isCollabAcrossRepos} = require("../../iscollab")
+const { getUserInfo } = require('../../checkGithubUser');
+const { owner } = require('../../config.json');
+const { roleId, collabUsersIds } = require('../../config.json')
+const { isCollabAcrossRepos } = require('../../iscollab')
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('verify')
 		.setDescription('pass in your github username to check if you qualify for contributor role!')
-        .addStringOption(option => option.setName('github-user')
-        .setDescription('add github user').setRequired(true)),
+		.addStringOption(option => option.setName('github-user')
+		.setDescription('add github user').setRequired(true)),
 	async execute(interaction) {
 		const discordID =interaction.user.id;
-        if(collabUsersIds.includes(discordID)){
+		if(collabUsersIds.includes(discordID)){
 			const role = interaction.guild.roles.cache.get(roleId);
 			const discordUser = interaction.guild.members.cache.get(discordID);
 			discordUser.roles.add(role);
-            await interaction.reply('You are already a Lichess Contributor! Contributor role granted! Thanks for contributing to Lichess.');
+			await interaction.reply({ content: 'You are already a Lichess Contributor! Contributor role granted! Thanks for contributing to Lichess.', ephemeral: true });
 		}else{
 
-        const member = interaction.options.getString('github-user');
+		const member = interaction.options.getString('github-user');
 		const determineDiscordVerification = await getUserInfo(member, discordID);
-		const checkCollabRepos = await isCollabAcrossRepos(owner, member);
 
 		if(determineDiscordVerification){
+			const checkCollabRepos = await isCollabAcrossRepos(owner, member);
 
 			if(checkCollabRepos){
-				 const role = interaction.guild.roles.cache.get(roleId);
-				 const discordUser = interaction.guild.members.cache.get(discordID);
-				 discordUser.roles.add(role);
-                 await interaction.reply('Contributor role granted! Thanks for contributing to Lichess!');
+				const role = interaction.guild.roles.cache.get(roleId);
+				const discordUser = interaction.guild.members.cache.get(discordID);
+				discordUser.roles.add(role);
+				await interaction.reply({ content: 'Contributor role granted! Thanks for contributing to Lichess!', ephemeral: true });
 			}else{
-				await interaction.reply('Contributor verification failed! Please contribute to Lichess to optain the role!');
+				await interaction.reply({ content: 'Contributor verification failed! Please contribute to Lichess to optain the role!', ephemeral: true });
 			}
 			
 		}else{
-			await interaction.reply(`Github owner verification failed! Please add ${discordID} in your Github bio and run **/verify** again!`);
+			await interaction.reply({ content: `Github owner verification failed! Please add ${discordID} in your Github bio and run **/verify** again!`, ephemeral: true });
 		}
 
-	}
-	
+		}
 	},
 };
